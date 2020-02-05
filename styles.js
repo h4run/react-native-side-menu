00675233a4cf34992ef8bbf7c0a9384767c99039
Harun <harun.memis@fabrikamedya.com.tr>
2020-02-05 15:09:50 +0300

    overlay styles changed

--- styles.js
+++ styles.js
@@ -1,33 +1,33 @@
 // @flow
 
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 const absoluteStretch = {
-  position: 'absolute',
+  position: "absolute",
   top: 0,
   left: 0,
   bottom: 0,
-  right: 0,
+  right: 0
 };
 
 export default StyleSheet.create({
   container: {
     ...absoluteStretch,
-    justifyContent: 'center',
+    justifyContent: "center"
   },
   menu: {
-    ...absoluteStretch,
+    ...absoluteStretch
   },
   frontView: {
     flex: 1,
-    position: 'absolute',
+    position: "absolute",
     left: 0,
     top: 0,
-    backgroundColor: 'transparent',
-    overflow: 'hidden',
+    backgroundColor: "transparent",
+    overflow: "hidden"
   },
   overlay: {
     ...absoluteStretch,
-    backgroundColor: 'transparent',
-  },
+    backgroundColor: "rgba(0,0,0,.6);"
+  }
 });
